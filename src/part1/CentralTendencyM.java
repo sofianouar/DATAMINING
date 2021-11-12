266c4d4efeb87e2e8e051c6638858ab44e4c7dbd
sopiie <soffianouar@gmail.com>
2021-11-12 15:33:51 +0100

    central tendecny metrics class
    added central tendecny metrics class

---
+++ src/part1/CentralTendencyM.java
@@ -0,0 +1,28 @@
+package part1;
+
+import java.util.ArrayList;
+
+public class CentralTendencyM {
+	ArrayList<Double> data;
+	int size;
+
+	public CentralTendencyM(ArrayList<Double> data) {
+	        this.data = data;
+	        this.size = data.size();
+	    }
+
+//moyenne
+	public double Mean(ArrayList<Double> data) {
+		
+		return 0;
+	}
+// moyenne tronqué
+
+//médiane
+	
+//mode 
+	
+//milieu de l'étendue
+	
+//déduire les symétries
+}
--- src/part1/main.java
+++ src/part1/main.java
@@ -2,6 +2,7 @@ package part1;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.jfree.data.xy.XYSeries;
@@ -9,40 +10,60 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 public class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		/*
+		 * ####1 : READING DATA
+		 */
+
 		System.out.println("Reading dataset...");
-		ArrayList<ArrayList<Double>> list = new ArrayList<>();
 
-		ArrayList<Double> lineAtt = new ArrayList<>();
-
-		BufferedReader bf = new BufferedReader(
-				new FileReader("C:\\Users\\soffi\\Documents\\M1 S1\\projects\\src\\seeds_dataset.txt"));
-
-		String str = bf.readLine();
-		String[] splitRow;
+		// Decalarations
 		int c = 0;
-		while (str != null) {
-			splitRow = str.split("\t+");
-			lineAtt = new ArrayList<>();
+		String[] splitRow;
+		ArrayList<Double> one_line = new ArrayList<>(); // contains all attributes of one specific line
+		ArrayList<ArrayList<Double>> list_data = new ArrayList<>(); // list of one_line
+
+		// reading the file seeds_data.txt
+		BufferedReader bf = new BufferedReader(
+				new FileReader("C:\\Users\\soffi\\Documents\\GitHub\\DATAMINING\\ressources\\seeds_dataset.txt"));
+
+		// reading all data lines : 1 2 3 4 5 6 7 8, 1 2 3 4 5 6 7 8 ....
+		String tmp_line = bf.readLine();
+
+		// spliting a specific line to separate the attributes :
+		// one_line=[1,2,3,4,5,6,7,8]
+		// parsing one_line's values to double : [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0]
+		// adding one_line to list_data :
+		// [[1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0],[1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0]....]
+
+		while (tmp_line != null) {
+			splitRow = tmp_line.split("\t+");
+			one_line = new ArrayList<>();
 
 			for (String s : splitRow) {
-				lineAtt.add(Double.parseDouble(s));
+				one_line.add(Double.parseDouble(s));
 			}
 
-			list.add(lineAtt);
+			list_data.add(one_line);
 			c++;
-			str = bf.readLine();
+			tmp_line = bf.readLine();
 
 		}
 
-		System.out.print(list);
+		// printing list
+		System.out.print(list_data);
+		System.out.println("Reading dataset ends ");
 
-//partie 2  diagramme de dispersion
+
+	}
+
+}
+
+
+/*
+ * partie 2  diagramme de dispersion
 		XYSeriesCollection colct = new XYSeriesCollection();
 		XYSeries serie = new XYSeries("");
 
 //JFreeChart charrt = ChartFactory.createScatterPlot("","","","");
-	}
-
-}
+ */
